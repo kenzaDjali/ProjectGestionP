a53d0c45399ab1a6259e12e64cef60f1ca50a2cc
Bouchra <b_ziraoui@hotmail.fr>
2014-10-24 12:14:14 +0200

    Template not done

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,38 @@
-<?php echo"Gestion de présence ";
+<?php 
+require_once'layout/header.php';
+require_once'layout/nav.php';
+?>
+<!-- /subnavbar -->
+
+<div class="main-inner">
+  <div class="container">
+    <div class="row">
+      <div class="span6">
+        test
+      </div>
+      <!-- /widget -->
+      <div class="widget widget-nopad">
+        <div class="widget-header"> <i class="icon-list-alt"></i>
+          <h3> Recent News</h3>
+        </div>
+        <!-- /widget-header -->
+        <div class="widget-content">
+          <div id='calendar' style ="height:250 ; width:538">
+
+          </div>
+        </div>
+        <!-- /widget-content --> 
+      </div>
+    </div>
+    <!-- /widget --> 
+  </div>
+  <!-- /span6 -->
+
+
+</div>
+<!-- /widget --> 
+
+
+
+
+  <?php require_once'layout/footer.php'; ?>
